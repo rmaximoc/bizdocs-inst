4ecd16f9720abefa9be23d8961ef5c0018ba9b03
Rodrigo <rodrigomaximocano@gmail.com>
2020-03-10 12:24:57 -0300

    Fix video position

--- src/components/IntroSection/IntroSection.style.js
+++ src/components/IntroSection/IntroSection.style.js
@@ -8,14 +8,16 @@ export const MarginBox = styled.div`
   display: flex;
   width: 80%;
   justify-content: center;
+  align-items: center;
   margin: 0 auto;
 `;
 
 export const TextContent = styled.div`
   display: flex;
   flex-direction: column;
-  width: 50%;
+  width: 100%;
   margin-bottom: 41px;
+  max-width: 550px;
 
   h1 {
     font-size: 36px;
@@ -30,9 +32,13 @@ export const TextContent = styled.div`
 export const PresentationContent = styled.div`
   display: flex;
   flex-direction: column;
-  margin: 130px 0 123px 0;
+  margin: 130px auto 123px;
+  width: 50%;
 `;
 
 export const VideoContent = styled.div`
   display: flex;
+  border: 1px solid red;
+  width: 564px;
+  height: 320px;
 `;
