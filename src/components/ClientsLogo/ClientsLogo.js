b89a84fc054df95da696c44c3d989a6397983627
Rodrigo <rodrigomaximocano@gmail.com>
2020-03-20 21:56:40 -0300

    Add client logo mobile

--- src/components/ClientsLogo/ClientsLogo.js
+++ src/components/ClientsLogo/ClientsLogo.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import { Container, MarginBox } from './ClientsLogo.style';
+import { Container, MarginBox, LogoBox } from './ClientsLogo.style';
 import logos from './logos';
 
 const ClientsLogo = () => {
   return (
     <Container>
       <MarginBox>
-        {logos.map(logo => (
-          <img src={logo} alt="logos" key={logo.toString()} />
-        ))}
+        <LogoBox>
+          {logos.map(logo => (
+            <img src={logo} alt="logos" key={logo.toString()} />
+          ))}
+        </LogoBox>
       </MarginBox>
     </Container>
   );
--- src/components/ClientsLogo/ClientsLogo.style.js
+++ src/components/ClientsLogo/ClientsLogo.style.js
@@ -10,9 +10,32 @@ export const MarginBox = styled.div`
   justify-content: space-around;
   align-items: center;
   margin: 0 auto 40px auto;
+`;
+
+export const LogoBox = styled.div`
+  display: grid;
+  object-fit: cover;
+  column-gap: 30px;
+  grid-template-columns: repeat(7, 1fr);
 
   img {
     max-width: 126px;
     max-height: 40px;
+
+    @media screen and (max-width: 992px) {
+      max-width: 83px;
+    }
+  }
+
+  @media screen and (max-width: 992px) {
+    grid-template-columns: repeat(5, 1fr);
+  }
+
+  @media screen and (max-width: 840px) {
+    grid-template-columns: repeat(4, 1fr);
+  }
+
+  @media screen and (max-width: 600px) {
+    grid-template-columns: repeat(3, 1fr);
   }
 `;
