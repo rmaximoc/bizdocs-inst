8ab5d4184187e33b48d6ca1114d8bf97da46c019
Rodrigo <rodrigomaximocano@gmail.com>
2020-03-25 20:09:42 -0300

    Fix scroll to top

--- src/routes.js
+++ src/routes.js
@@ -11,40 +11,43 @@ import Security from './pages/Security/Security';
 import News from './pages/News/News';
 import OpenedNews from './pages/OpenedNews/OpenedNews';
 import theme from './config/theme';
+import ScrollToTop from './utils/toTop';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <Router>
-        <Switch>
-          <Route path="/" exact>
-            <Home />
-          </Route>
-          <Route path="/bizdocs">
-            <Bizdocs />
-          </Route>
-          <Route path="/empresas">
-            <Empresas />
-          </Route>
-          <Route path="/plans/plan-details">
-            <PlanDetails />
-          </Route>
-          <Route path="/plans/benefits">
-            <Benefits />
-          </Route>
-          <Route path="/plans-and-prices">
-            <PlansAndPrices />
-          </Route>
-          <Route path="/security">
-            <Security />
-          </Route>
-          <Route path="/news">
-            <News />
-          </Route>
-          <Route path="/opened-news">
-            <OpenedNews />
-          </Route>
-        </Switch>
+        <ScrollToTop>
+          <Switch>
+            <Route path="/" exact>
+              <Home />
+            </Route>
+            <Route path="/bizdocs">
+              <Bizdocs />
+            </Route>
+            <Route path="/empresas">
+              <Empresas />
+            </Route>
+            <Route path="/plans/plan-details">
+              <PlanDetails />
+            </Route>
+            <Route path="/plans/benefits">
+              <Benefits />
+            </Route>
+            <Route path="/plans-and-prices">
+              <PlansAndPrices />
+            </Route>
+            <Route path="/security">
+              <Security />
+            </Route>
+            <Route path="/news">
+              <News />
+            </Route>
+            <Route path="/opened-news">
+              <OpenedNews />
+            </Route>
+          </Switch>
+        </ScrollToTop>
       </Router>
     </ThemeProvider>
   );
